217fec2dd4093fb1fd9510c8d8bb1c5d1095706e
Dario Leunig <daleunig@gmail.com>
2018-05-30 14:51:51 +0200

    Umstieg auf Swagger

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-const express = require('express')
-const app = express()
+'use strict';
 
-app.use(express.static('public'))
+var SwaggerExpress = require('swagger-express-mw');
+var app = require('express')();
+module.exports = app; // for testing
 
-app.listen(3000, () => console.log('Server running on port 3000'))
+var config = {
+  appRoot: __dirname // required config
+};
+
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+  if (err) { throw err; }
+
+  // install middleware
+  swaggerExpress.register(app);
+
+  var port = process.env.PORT || 3000;
+  app.listen(port);
+
+  if (swaggerExpress.runner.swagger.paths['/hello']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  }
+});
