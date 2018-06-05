b35ef9a98a175b87f420840e269bc2f0dff6afd6
Dario Leunig <daleunig@gmail.com>
2018-06-05 16:02:42 +0200

    Anpassung fuer pullen vom Server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       user: 'ubuntu',
       host: 'ec2-18-218-110-87.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/ludo.pem',
-      ref: 'origin/swagger',
+      ref: 'origin/master',
       repo: 'git@github.com:SoftwareprojektGR4/Anwendungslogik.git',
       path: '/home/ubuntu/nodejs',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
