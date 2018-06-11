feb03ab20920b84eecc228006188a19fe1cce840
Dario Leunig <daleunig@gmail.com>
2018-06-11 11:18:14 +0200

    Datenbank Verbindung Test #1

---
+++ api/controllers/game.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var fs = require('fs');
+var app = express();
+
+// load all model files
+fs.readdirSync(__dirname+ '/models').forEach(function(filename) {
+	if(~filename.indexOf('.js')) require(__dirname + '/models/' + filename);
+});
+
+// Database Connection
+mongoose.connect('mongodb://localhost/ludodb');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+});
+
+
+/*
+	Returns all games from database ludodb
+*/
+function getGames(request, response) {
+	mongoose.model('game').find(function(err, games) {
+		console.log(games);
+	});
+}
+
+module.exports = {
+  getGames: getGames
+};
