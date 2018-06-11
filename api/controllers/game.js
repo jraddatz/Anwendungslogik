11bb010699be2cbf381d2553d111e48b9e4dcf6c
Dario Leunig <daleunig@gmail.com>
2018-06-11 12:09:19 +0200

    Test #4

--- api/controllers/game.js
+++ api/controllers/game.js
@@ -1,30 +1,4 @@
-/*
-var express = require('express');
-var mongoose = require('mongoose');
-var fs = require('fs');
-var app = express();
-
-// load all model files
-fs.readdirSync(__dirname+ '/models').forEach(function(filename) {
-	if(~filename.indexOf('.js')) require(__dirname + '/models/' + filename);
-});
-
-// Database Connection
-mongoose.connect('mongodb://localhost/ludodb');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-});
-*/
-
-
-/*
-	Returns all games from database ludodb
-*/
 function getGames(request, response) {
-	/*mongoose.model('game').find(function(err, games) {
-		console.log(games);
-	});*/
 	response.json({id: 3});
 }
 
