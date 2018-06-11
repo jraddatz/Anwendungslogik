247047e9aaccd0b237f622575777b8f021cb5dd6
Dario Leunig <daleunig@gmail.com>
2018-06-11 12:30:12 +0200

    Test #8

--- api/controllers/game.js
+++ api/controllers/game.js
@@ -1,6 +1,19 @@
-var data = {
-	id: 1
-}
+var data = [
+  {
+    "id": 1,
+    "name": "labore in",
+    "description": "eiusmod enim Lorem deserunt",
+    "maxPlayers": 6,
+    "minPlayers": 2
+  },
+  {
+    "id": 2,
+    "name": "lorem ipsum",
+    "description": "bla bla bla",
+    "maxPlayers": 8,
+    "minPlayers": 1
+  },
+]
 
 function getGames(request, response) {
 	response.json(data);
