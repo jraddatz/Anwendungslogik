df87bd92ef9cdf7e9a1f9e576329ac9f1698cf1f
Dario Leunig <daleunig@gmail.com>
2018-06-11 11:30:58 +0200

    Test #2

--- api/controllers/game.js
+++ api/controllers/game.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express');
 var mongoose = require('mongoose');
 var fs = require('fs');
@@ -14,15 +15,17 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
 });
+*/
 
 
 /*
 	Returns all games from database ludodb
 */
 function getGames(request, response) {
-	mongoose.model('game').find(function(err, games) {
+	/*mongoose.model('game').find(function(err, games) {
 		console.log(games);
-	});
+	});*/
+	response.json("Hallo Welt!");
 }
 
 module.exports = {
